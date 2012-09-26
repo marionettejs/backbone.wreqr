00a788a2f07cd263a5794c29608435a5b298a280
Derick Bailey <derickbailey@gmail.com>
2012-09-26 16:20:33 -0500

    more specs for handlers

--- spec/javascripts/handlers.spec.js
+++ spec/javascripts/handlers.spec.js
@@ -2,43 +2,85 @@ describe("handlers", function(){
 
   describe("when requesting a handler by name", function(){
 
-    describe("and a handler has been registered with that name", function(){
-      var handler, registeredHandler, ctx;
+    describe("and a handler has been addHandlered with that name", function(){
+      var handler, addHandleredHandler, ctx;
 
       beforeEach(function(){
         var handlers = new Wreqr.Handlers();
 
         ctx = {};
-        registeredHandler = jasmine.createSpy("a registered handler");
-        handlers.register("handler", registeredHandler, ctx);
+        addHandleredHandler = jasmine.createSpy("a addHandlered handler");
+        handlers.addHandler("handler", addHandleredHandler, ctx);
 
         handler = handlers.getHandler("handler");
+        handler();
       });
 
-      it("should return the registered handler callback", function(){
-        expect(handler.callback).toBe(registeredHandler);
+      it("should return the addHandlered handler callback", function(){
+        expect(addHandleredHandler).toHaveBeenCalled();
       });
 
-      it("should return the registered handler context", function(){
-        expect(handler.context).toBe(ctx);
+      it("should return the addHandlered handler context", function(){
+        expect(addHandleredHandler.mostRecentCall.object).toBe(ctx);
       });
 
     });
 
-    describe("and a handler has not been registered with that name", function(){
+    describe("and a handler has not been addHandlered with that name", function(){
       var handlers;
 
       beforeEach(function(){
         handlers = new Wreqr.Handlers();
       });
 
-      it("should return the registered handler", function(){
+      it("should thrown an error saying a handler was not found", function(){
         function getHandler(){ handlers.getHandler("not registered");}
+
         expect(getHandler).toThrow("Handler not found for 'not registered'");
       });
 
     });
 
+  });
+
+  describe("when removing a named handler", function(){
+    var handlers, addHandleredHandler, ctx;
+
+    beforeEach(function(){
+      handlers = new Wreqr.Handlers();
+
+      ctx = {};
+      addHandleredHandler = jasmine.createSpy("a addHandlered handler");
+      handlers.addHandler("handler", addHandleredHandler, ctx);
+      handlers.removeHandler("handler");
+    });
+
+    it("should no longer return the requested hander", function(){
+      function getHandler(){ handlers.getHandler("handler");}
+
+      expect(getHandler).toThrow("Handler not found for 'handler'");
+    });
+  });
+
+  describe("when removing all handlers", function(){
+    var handlers, addHandleredHandler, ctx;
+
+    beforeEach(function(){
+      handlers = new Wreqr.Handlers();
+
+      ctx = {};
+      addHandleredHandler = jasmine.createSpy("a addHandlered handler");
+      handlers.addHandler("handler1", addHandleredHandler, ctx);
+      handlers.addHandler("handler2", addHandleredHandler, ctx);
+
+      handlers.removeAllHandlers();
+    });
+
+    it("should no longer return the requested handler", function(){
+      function getHandler(){ handlers.getHandler("handler");}
+
+      expect(getHandler).toThrow("Handler not found for 'handler'");
+    });
   });
 
 });
--- src/wreqr.handlers.js
+++ src/wreqr.handlers.js
@@ -4,7 +4,7 @@ Wreqr.Handlers = function(){
 };
 
 _.extend(Wreqr.Handlers.prototype, {
-  register: function(name, handler, context){
+  addHandler: function(name, handler, context){
     var config = {
       callback: handler,
       context: context
@@ -13,21 +13,23 @@ _.extend(Wreqr.Handlers.prototype, {
     this._handlers[name] = config;
   },
 
-  getHandler: function(name, args){
+  getHandler: function(name){
     var config = this._handlers[name];
 
     if (!config){
       throw new Error("Handler not found for '" + name + "'");
     }
 
-    return config;
+    return function(){
+      return config.callback.apply(config.context, arguments);
+    }
   },
 
   removeHandler: function(name){
     delete this._handlers[name];
   },
 
-  clearHandlers: function(){
+  removeAllHandlers: function(){
     this._handlers = {};
   }
 });
