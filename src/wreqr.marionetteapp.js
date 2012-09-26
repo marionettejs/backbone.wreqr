95cafb8a9e7c3146f8f7affe8bfbf4aa09df693a
Derick Bailey <derickbailey@gmail.com>
2012-09-26 16:40:55 -0500

    augment Marionette.Application with request and execute methods, if Marionette.Application exists

---
+++ src/wreqr.marionetteapp.js
@@ -0,0 +1,16 @@
+if (Backbone && Backbone.Marionette && Backbone.Marionette.Application){
+  var commands = new Wreqr.Commands();
+  var reqres = new Wreqr.RequestResponse();
+
+  _.extend(Backbone.Marionette.Application.prototype, {
+    commands: commands,
+    execute: function(name, argObj){
+      commands.execute(name, argObj);
+    },
+
+    requestResponse: reqres,
+    request: function(name, argObj){
+      return commands.request(name, argObj);
+    }
+  });
+}
