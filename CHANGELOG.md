cf6eff90076a58da50193b099e4a8567004dd98f
Derick Bailey <derickbailey@gmail.com>
2012-12-20 08:58:10 -0600

    replaced EventAggregator with a version that is based on Backbone.Events from backbone v0.9.9

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Change log
 
+### v0.2.0
+
+* **BREAKING:** Replaced EventAggregator implementation with Backbone.Events use, providing context binding through listenTo
+
 ### v0.1.0
 
 * Fix calls to `.apply` to account for IE < 9 throwing an error when `arguments` is null or undefined
--- src/wreqr.eventaggregator.js
+++ src/wreqr.eventaggregator.js
@@ -4,14 +4,30 @@
 // of an application through event-driven architecture.
 
 Wreqr.EventAggregator = (function(Backbone, _){
-  "option strict";
-  var EA = function(){};
 
-  // Copy the `extend` function used by Backbone's classes
-  EA.extend = Backbone.Model.extend;
+  // Grab a reference to the original listenTo
+  var listenTo = Backbone.Events.listenTo;
 
-  // Copy the basic Backbone.Events on to the event aggregator
-  _.extend(EA.prototype, Backbone.Events);
+  // Create a version of listenTo that allows contexting binding
+  function contextBoundListenTo(obj, evtSource, events, callback, context){
+    context = context || obj;
+    return listenTo.call(obj, evtSource, events, _.bind(callback, context));
+  }
 
-  return EA;
+  // Define the EventAggregator
+  function EventAggregator(){}
+
+  // Mix Backbone.Events in to it
+  _.extend(EventAggregator.prototype, Backbone.Events, {
+    // Override the listenTo so that we can have a version that
+    // correctly binds context
+    listenTo: function(evtSource, events, callback, context){
+      return contextBoundListenTo(this, evtSource, events, callback, context);
+    }
+  });
+
+  // Allow it to be extended
+  EventAggregator.extend = Backbone.Model.extend;
+
+  return EventAggregator;
 })(Backbone, _);
